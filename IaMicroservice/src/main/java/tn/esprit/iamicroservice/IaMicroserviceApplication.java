ccad8a9d1fff2b35e281d9e4d1d1e0c2ae0d23e5
oussema-hmaied5 <oussama.hamaied@hotmail.fr>
2023-10-13 16:50:39 +0100

    second commit

---
+++ IaMicroservice/src/main/java/tn/esprit/iamicroservice/IaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.iamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IaMicroserviceApplication.class, args);
+    }
+
+}
---
+++ IaMicroservice/src/test/java/tn/esprit/iamicroservice/IaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.iamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
