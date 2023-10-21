4b258764943bf0544a9d3720ec1339e1bad2739d
Souhe <souha.jaidi@esprit.tn>
2023-10-21 11:48:14 +0100

    Fonctions_avancée

--- MS_Event/src/main/java/tn/esprit/ms_event/MsEventApplication.java
+++ MS_Event/src/main/java/tn/esprit/ms_event/MsEventApplication.java
@@ -2,12 +2,16 @@ package tn.esprit.ms_event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class MsEventApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsEventApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
